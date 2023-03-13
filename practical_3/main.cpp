96e648d8525d86779f2ca37667e5682690eb4d46
RossMuir98 <40482372@live.napier.ac.uk>
2023-03-13 16:36:50 +0000

    Tile engine complete

--- practical_3/main.cpp
+++ practical_3/main.cpp
@@ -1,25 +1,125 @@
-#include <SFML/Graphics.hpp>
+#include "Player.h"
+#include "LevelSystem.h"
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 using namespace sf;
 
+const int gameWidth = 1920;
+const int gameHeight = 1080;
+unique_ptr<Player> player(new Player());
 
+void load()
+{
 
-int main(){
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+    auto path = "res/levels/maze.txt";
 
-  while (window.isOpen()){
-      sf::Event event;
-      while (window.pollEvent(event)){
-      if (event.type == sf::Event::Closed){
-        window.close();
-      }
+    ls::loadLevelFile(path);
+
+    string buffer;
+
+    ifstream f(path);
+    if (f.good()) {
+        f.seekg(0, std::ios::end);
+        buffer.resize(f.tellg());
+        f.seekg(0);
+        f.read(&buffer[0], buffer.size());
+        f.close();
     }
-    window.clear();
-    window.draw(shape);
-    window.display();
-  }
-  return 0;
+    else {
+        throw string("Couldn't open level file: ") + path;
+    }
+
+    size_t pos[] = { 0,0 };
+
+    auto row = 0;
+
+    for (int i = 0; i < buffer.size(); i++)
+    {
+        if (buffer[i] == 's')
+        {
+            pos[0] = i % row;
+            pos[1] = row;
+            break;
+        }
+
+        if (buffer[i] == '\n')
+        {
+            row++;
+        }
+    }
+
+    cout << pos[0] << ", " << pos[1] << endl;
+
+    auto s = ls::getTilePosition({ pos[0], pos[1] });
+
+    auto poo = ls::getHeight();
+
+    auto poopoo = ls::getWidth();
+
+    s.x += 50;
+    s.y += 50;
+
+    player->setPosition(s);
+
+    cout << s.x << ", " << s.y << endl;
+
+    // Print the level to the console
+    for (size_t y = 0; y < ls::getHeight(); ++y) {
+        for (size_t x = 0; x < ls::getWidth(); ++x) {
+            cout << ls::getTile({ x, y });
+        }
+        cout << endl;
+    }
+}
+
+void Update(RenderWindow& window)
+{
+    // Reset clock, recalculate deltatime
+    static Clock clock;
+    float dt = clock.restart().asSeconds();
+
+    // check and consume events
+    Event event;
+    while (window.pollEvent(event))
+    {
+        if (event.type == Event::Closed)
+        {
+            window.close();
+            return;
+        }
+    }
+
+    // Quit Via ESC Key
+    if (Keyboard::isKeyPressed(Keyboard::Escape))
+    {
+        window.close();
+    }
+
+    player->Update(dt);
+}
+
+void Render(RenderWindow& window)
+{
+    ls::Render(window);
+    player->Render(window);
+}
+
+int main() {
+    RenderWindow window(VideoMode({ gameWidth, gameHeight }), "SpaceInvaders");
+    View view;
+    view.reset(FloatRect({ 100, 100 }, { 1920, 1080 }));
+    window.setView(view);
+    load();
+    while (window.isOpen()) {
+        window.clear();
+        //view.setCenter(player.get()->getPosition());
+        Update(window);
+        window.setView(view);
+        view.setCenter(player.get()->getPosition());
+        Render(window);
+        window.display();
+    }
+    return 0;
 }
--- practical_3/player.cpp
+++ practical_3/player.cpp
@@ -1,11 +1,12 @@
-//player.cpp
 #include "player.h"
+#include <iostream>
+
 using namespace sf;
 using namespace std;
 
 void Player::Update(double dt) {
-  //Move in four directions based on keys
-  
+    //Move in four directions based on keys
+
     if (Keyboard::isKeyPressed(Keyboard::A))
     {
         move(Vector2f((dt * -_speed), 0));
@@ -22,16 +23,37 @@ void Player::Update(double dt) {
     {
         move(Vector2f(0, (dt * _speed)));
     }
-	
-  Entity::Update(dt);
+
+    if (Mouse::isButtonPressed(Mouse::Left))
+    {
+        auto pos = Mouse::getPosition();
+        auto current = getPosition();
+
+        printf("%f", pos.x);
+
+        auto dif = pos;
+
+        dif.x -= current.x;
+        dif.y -= current.y;
+
+        auto length = sqrtf((dif.x * dif.x) + (dif.y * dif.y));
+
+        dif.x = dif.x / length;
+        dif.y = dif.y / length;
+
+        move(Vector2f((dif.x * dt * _speed), (dif.y * dt * _speed)));
+
+    }
+
+    Entity::Update(dt);
 }
 
 Player::Player()
     : _speed(200.0f), Entity(make_unique<CircleShape>(25.f)) {
-  _shape->setFillColor(Color::Magenta);
-  _shape->setOrigin(Vector2f(25.f, 25.f));
+    _shape->setFillColor(Color::Magenta);
+    _shape->setOrigin(Vector2f(25.f, 25.f));
 }
 
-void Player::Render(sf::RenderWindow &window) const {
-  window.draw(*_shape);
+void Player::Render(sf::RenderWindow& window) const {
+    window.draw(*_shape);
 }
